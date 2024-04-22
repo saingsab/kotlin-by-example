74121e64f60c4fd1a5496a96e53bae376ba48827
saing <saing.sab@gmail.com>
2024-04-22 15:51:21 +0700

    controll flow

---
+++ src/main/kotlin/ControlFlow/When.kt
@@ -0,0 +1,21 @@
+package org.example.ControlFlow
+
+fun main() {
+    cases("Hello")
+    cases(1)
+    cases(0L)
+    cases(MyClasses())
+    cases("hello")
+}
+
+fun cases(obj: Any) {
+    when (obj) {
+        1 -> println("One")
+        "Hello" -> println("Greeting")
+        is Long -> println("Long")
+        !is String -> println("Not a string")
+        else -> println("Unknown")
+    }
+}
+
+class MyClasses
